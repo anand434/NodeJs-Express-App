d0e62b0b0af92ca76c72503d0c1534e37fbd8308
anand434 <kr.anand.srm@gmail.com>
2021-06-26 23:19:25 +0530

    added async, delete, patch

--- routes/posts.js
+++ routes/posts.js
@@ -3,25 +3,75 @@ const Post = require('../models/post');
 const router = express.Router();
 
 
-router.get(
-    '/', (req, res) => {
-        res.send('this is post');
+// get specific post
+router.get('/api/:postId', async (req, res) => {
+    try {
+        const ppost = await Post.findById(req.params.postId);
+        res.json(ppost);
+    }
+    catch (err){ 
+        res.json({message: err});
+    }
+});
+
+// Update Post
+router.patch('/api/:postId', async (req, res) => {
+    try {
+        const updateOp = await Post.update({_id: req.params.postId}, {
+            $set : {
+                title: req.body.title,
+                descripton: req.body.descripton
+            }
+        });
+        res.json(updateOp);
+    }
+    catch(err) {
+        res.json({ message: err});
+    }
+});
+
+// Delete Post
+router.delete('/api/:postId', async (req, res) => {
+    try {
+        const delOp = await Post.remove({_id: req.params.postId});
+        res.json(delOp);
+    }
+    catch(err) {
+        res.json({ message: err});
+    }
+});
+
+
+// get all posts
+router.get( '/api', 
+    async (req, res) => {
+        try {
+            const posts = await Post.find();
+            res.json(posts);
+        }
+        catch(err) {
+            res.json({ message: err});
+        }
+    
     }
 );
 
-router.post('/api', (req, res) => {
+
+// submit a post
+router.post('/api', async (req, res) => {
     const post = new Post({
         title: req.body.title,
         descripton: req.body.descripton
     });
 
-    post.save()
-    .then(data => {
-        res.json(data);
-    })
-    .catch(err => {
-        res.json({message:err});
-    })
+    try {
+        const saveedPost = await post.save();
+        res.json(saveedPost);
+    }
+    catch(err) {
+        res.json({ message: err});
+    }
+
 });
 
 
